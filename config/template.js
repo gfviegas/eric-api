e006f27ae1832767014b6304211d1bf0e09a9b12
Gustavo Viegas <webviegas@gmail.com>
2017-04-11 09:01:57 -0300

    Fixed status message

--- config/template.js
+++ config/template.js
@@ -5,7 +5,22 @@ const email = {
   address: 'Rua Mariano Procópio, 90 - Mariano Procópio, Juiz de Fora - Minas Gerais',
   phone: '(32) 3215-7674',
   logo: `${process.env.ASSETS_URL}img/logo.png`,
-  imgPath: `${process.env.ASSETS_URL}img/`
+  imgPath: `${process.env.ASSETS_URL}img/`,
+  rewardStatus: (status) => {
+    if (!status) return ''
+    switch (status) {
+      case 'waiting':
+        return 'Aguardando - A solicitação foi recebida e está aguardando análise.'
+      case 'analyzing':
+        return 'Analisando - A Região Escoteira está analisando a sua solicitação.'
+      case 'issued':
+        return 'Emitido - A solicitação foi aprovada e a sua recompensa já foi emitida.'
+      case 'rejected':
+        return 'Indeferido - A solicitação foi negada.'
+      case 'approved':
+        return 'Deferido - A solicitação foi aprovada e aguarda emissão.'
+    }
+  }
 }
 
 const configure = (app) => {
